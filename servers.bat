83d9262b778ac756948fa90edd00e56df366cfb3
railgit <68155250+railgit@users.noreply.github.com>
2020-11-22 15:59:17 +0530

    Delete servers.bat

--- servers.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-start cmd.exe /K "cd C:\Djangular\backend && Scripts\activate && cd rail && python manage.py runserver" 
-start cmd.exe /K "cd C:\Djangular\frontend\railfront && ng serve" 
-
-
